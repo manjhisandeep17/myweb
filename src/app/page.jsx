aabe6539bf2b0883e6e737968d566ce579922904
sandy <manjhisandeep17@gmail.com>
2024-03-21 16:14:32 +0530

    Initial commit from Create Next App

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import {  LampDemo } from "@/components/ui/lamp";
+
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+      <LampDemo></LampDemo>
+    </main>
+  );
+}
